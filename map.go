19fd29dcc21485848cd70a93a53fd1e0f74c72c2
王海明 <wanghaiming@yaochufa.com>
2020-11-05 18:08:45 +0800

    commit map demo

---
+++ map.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	m := make(map[string]int)
+	m["k1"] = 1
+	m["k2"] = 20
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("m", m)
+
+	_, prs := m["k2"]
+	fmt.Println("has key k2 ?:", prs)
+
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+}
