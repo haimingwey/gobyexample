9a3d414ad777b833f54396c1612b9010dd03badf
王海明 <wanghaiming@yaochufa.com>
2020-11-10 17:54:29 +0800

    commit

---
+++ execing-processes/execing-processes.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+
+	binary, lookErr := exec.LookPath("ls")
+	if lookErr != nil {
+		panic(lookErr)
+	}
+	args := []string{"ls", "-a", "-l", "-h"}
+
+	env := os.Environ()
+
+	exeErr := syscall.Exec(binary, args, env)
+	if exeErr != nil {
+		panic(exeErr)
+	}
+
+}
